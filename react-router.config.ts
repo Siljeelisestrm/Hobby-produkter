dcb182669e935d02b904ef1461a5030fdfc66edf
Silje Elise Strøm <silje@smileycom.com>
2026-09-11 14:00:38 +0200

    Fix Pages build and fallback handling

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,5 +2,6 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   basename: "/Hobby-produkter",
+  prerender: ["/", "/legg-til-produkt"],
   ssr: true,
 } satisfies Config;
